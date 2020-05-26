0d705a939893f06a2ad7735ceb12062fa632c903
pedro.silva <pedro.p.silva.ext@spms.min-saude.pt>
2020-05-26 21:47:28 +0100

    chore: add jest to lint staged

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
